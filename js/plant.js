109309836a99028c369c9e111480da7e44f3d7d9
Sarun Peetasai <hamgmm@gmail.com>
2012-12-27 00:20:30 +0700

    Change Menu Color,Center Object

--- js/plant.js
+++ js/plant.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 			
 			$('#iwrapper').css({height:+$height+'px'});
 			$('#iwrapper').css({width:+$width+'px'});
-			$('#index_plant').css({margin:'0 0 0 '+(($width / 2)-156.5)+'px'});
+			$('#index_plant').css({margin:'0 0 0 '+(($width / 2)-140)+'px'});
 			$('#logo').css({margin:'75px 0 0 '+(($width / 2)-125)+'px'});
 			
 });
