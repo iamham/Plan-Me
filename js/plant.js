42f245448dd939898e4bb04eeddd037fc62f32ce
Sarun Peetasai <hamgmm@gmail.com>
2012-12-26 20:25:07 +0700

    First Rev Uploaded

---
+++ js/plant.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+$(document).ready(function() {
+			$height = $(window).height();
+			$width = $(window).width();
+			
+			$('#iwrapper').css({height:+$height+'px'});
+			$('#iwrapper').css({width:+$width+'px'});
+			$('#index_plant').css({margin:'0 0 0 '+(($width / 2)-156.5)+'px'});
+			$('#logo').css({margin:'75px 0 0 '+(($width / 2)-208.5)+'px'});
+			
+});
