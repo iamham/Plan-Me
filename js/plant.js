3d2c26c25399d4c92df160f156790d2de27c610f
Sarun Peetasai <hamgmm@gmail.com>
2012-12-26 22:14:08 +0700

    DPI Change with minimum = 1

--- js/plant.js
+++ js/plant.js
@@ -6,6 +6,6 @@ $(document).ready(function() {
 			$('#iwrapper').css({height:+$height+'px'});
 			$('#iwrapper').css({width:+$width+'px'});
 			$('#index_plant').css({margin:'0 0 0 '+(($width / 2)-156.5)+'px'});
-			$('#logo').css({margin:'75px 0 0 '+(($width / 2)-175)+'px'});
+			$('#logo').css({margin:'75px 0 0 '+(($width / 2)-208.5)+'px'});
 			
 });
